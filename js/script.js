139befc12fb3b2fa695d345493028fc8c4adb7df
Sebastian Gubiec <seb.gub@wp.pl>
2021-06-13 23:52:22 +0200

    end6.3

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
   console.log('links:', links);
 });*/
 
-const titleClickHandler = function(event){
+const titleClickHandler = function(event) {
   event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
@@ -14,7 +14,7 @@ const titleClickHandler = function(event){
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-  for(let activeLink of activeLinks){
+  for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
@@ -26,20 +26,25 @@ const titleClickHandler = function(event){
 
   const activeArticles = document.querySelectorAll('.posts .active');
 
-  for(let activeArticle of activeArticles){
+  for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
-  const articleSelector = document.getElementById('article');
-  const href = articleSelector.getAttribute('href');
+
+  const articleSelector = clickedElement.getAttribute('href');
   console.log('Article:', articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('target:', targetArticle);
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
