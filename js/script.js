f66473f5d4f0ecb785ea701c5ac16b56f0223657
Sebastian Gubiec <seb.gub@wp.pl>
2021-06-15 22:08:54 +0200

    remove_contents

--- js/script.js
+++ js/script.js
@@ -48,3 +48,32 @@ const links = document.querySelectorAll('.titles a');
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
+
+{
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  console.log (titleList);
+  titleList.innerHTML = '';
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
+}
